8c579fb64030f9a5ee6f07a80b26202e12355f01
Mibbs <colinoneill1992@gmail.com>
2013-12-04 22:29:31 -0500

    asdf

--- CS4400/NewClient.cs
+++ CS4400/NewClient.cs
@@ -27,35 +27,42 @@ namespace CS4400
 
         private void CreateClientClicked(object sender, EventArgs e)
         {
-            Program.OpenConnection();
-            String dob = DOB.Value.ToString("yyyyMMdd");
-            String start = Start.Value.ToString("yyyyMMdd");
-            String insert = String.Format("INSERT INTO Client(Gender, DOB, Start, PDay, FirstName, LastName, Phone, Street, City, State, Zip, Apt, BagName) VALUES (\"{0}\", \"{1}\",\" {2}\",{3}, \"{4}\", \"{5}\",\"{6}\", \"{7}\", \"{8}\", \"{9}\", {10}, \"{11}\", \"{12}\")", Gender.Text, dob, start, PickUpDay.Text, FirstName.Text, LastName.Text, Telephone.Text, Street.Text, City.Text, State.Text, Zip.Text, AptNum.Text, BagName.SelectedItem.ToString());
-            MySqlCommand cmd = new MySqlCommand(insert, Program.connection);
-            cmd.ExecuteNonQuery();
-            string grabCID =  "SELECT MAX(CID) FROM Client";
-            cmd = new MySqlCommand(grabCID, Program.connection);
-            int CID = Convert.ToInt32(cmd.ExecuteScalar());
-            if (CID != null & CID != 0)
+            try
             {
-                clientCID = CID;
-
-                String haveAid = String.Format("INSERT INTO HasAid(FinAidName, CID) VALUES (\"{0}\",{1})", FinAid.SelectedItem.ToString(), clientCID);
-                cmd = new MySqlCommand(haveAid, Program.connection);
+                Program.OpenConnection();
+                String dob = DOB.Value.ToString("yyyyMMdd");
+                String start = Start.Value.ToString("yyyyMMdd");
+                String insert = String.Format("INSERT INTO Client(Gender, DOB, Start, PDay, FirstName, LastName, Phone, Street, City, State, Zip, Apt, BagName) VALUES (\"{0}\", \"{1}\",\" {2}\",{3}, \"{4}\", \"{5}\",\"{6}\", \"{7}\", \"{8}\", \"{9}\", {10}, \"{11}\", \"{12}\")", Gender.Text, dob, start, PickUpDay.Text, FirstName.Text, LastName.Text, Telephone.Text, Street.Text, City.Text, State.Text, Zip.Text, AptNum.Text, BagName.SelectedItem.ToString());
+                MySqlCommand cmd = new MySqlCommand(insert, Program.connection);
                 cmd.ExecuteNonQuery();
+                string grabCID = "SELECT MAX(CID) FROM Client";
+                cmd = new MySqlCommand(grabCID, Program.connection);
+                int CID = Convert.ToInt32(cmd.ExecuteScalar());
+                if (CID != null & CID != 0)
+                {
+                    clientCID = CID;
 
-                Program.CloseConnection();
+                    String haveAid = String.Format("INSERT INTO HasAid(FinAidName, CID) VALUES (\"{0}\",{1})", FinAid.SelectedItem.ToString(), clientCID);
+                    cmd = new MySqlCommand(haveAid, Program.connection);
+                    cmd.ExecuteNonQuery();
 
-                //show the family member form
-                FamilyMembers famForm = new FamilyMembers();
-                famForm.MdiParent = Program.MainForm;
-                famForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-                Program.NewClient.Hide();
-                famForm.Show();
+                    Program.CloseConnection();
+
+                    //show the family member form
+                    FamilyMembers famForm = new FamilyMembers();
+                    famForm.MdiParent = Program.MainForm;
+                    famForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+                    Program.NewClient.Hide();
+                    famForm.Show();
+                }
+                else
+                {
+                    Program.CloseConnection();
+                }
             }
-            else 
+            catch
             {
-                Program.CloseConnection();
+                DialogResult error = MessageBox.Show("All fields must be filled!");
             }
 
         }
